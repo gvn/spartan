7f71ef5f749757e2c3af2da5d97189d4fe44aa6b
gvn <gavin@wetcat.org>
2012-02-07 12:54:23 -0800

    first

---
+++ _ui/js/main.js
@@ -0,0 +1,12 @@
+// TODO: Replace NAMESPACE with the site name in all caps
+
+NAMESPACE = {
+	init:function() {
+		var self = this;
+		
+	}
+};
+
+$(document).ready(function() {
+	NAMESPACE.init();
+});
