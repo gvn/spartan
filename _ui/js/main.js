becfc6123d749ed7019e93b80709f666d402bfaf
Gavin Suntop <gavin@gsuntop.com>
2012-02-07 14:31:57 -0800

    Removing cruft, adding Google hosted jQuery.

--- _ui/js/main.js
+++ _ui/js/main.js
@@ -1,6 +1,10 @@
 // TODO: Replace NAMESPACE with the site name in all caps
 
-NAMESPACE = {
+if (typeof window.NAMESPACE !== 'undefined') {
+	throw 'NAMESPACE already in use.';
+}
+
+window.NAMESPACE = {
 	init:function() {
 		var self = this;
 		
