ae679922129fe76630aca705738d39cebf25c5b4
GVN <gavin@gsuntop.com>
2012-08-07 17:48:05 -0700

    Update _ui/js/main.js

--- _ui/js/main.js
+++ _ui/js/main.js
@@ -1,16 +1,15 @@
 // TODO: Replace NAMESPACE with something meaningful
 
 if (typeof window.NAMESPACE !== 'undefined') {
-	throw 'NAMESPACE already in use.';
+    throw 'NAMESPACE already in use.';
 }
 
 window.NAMESPACE = {
-	init: function () {
-		var self = this;
-
-	}
+    init: function () {
+        var self = this;
+    }
 };
 
 $(document).ready(function () {
-	NAMESPACE.init();
+    NAMESPACE.init();
 });
